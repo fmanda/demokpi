6d005d6fa477bd7ce83ce5bb49024875a00aaf4e
fmanda <febrian.manda@gmail.com>
2020-07-17 19:17:02 +0700

    -

--- server/src/routes/kpidept.php
+++ server/src/routes/kpidept.php
@@ -1,13 +1,14 @@
 <?php
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\UploadedFileInterface;
 
 require '../vendor/autoload.php';
 require_once '../src/classes/DB.php';
 require_once '../src/models/ModelKPIDept.php';
 
-$container = $app->getContainer();
-$directory = __DIR__ . '/uploads';
+// $container = new Container();
+// $container->set('upload_directory', __DIR__ . '/uploads');
 
 $app->get('/kpidept/{dept_id}/{year}', function ($request, $response, $args) {
 	try{
@@ -27,20 +28,30 @@ $app->get('/kpidept/{dept_id}/{year}', function ($request, $response, $args) {
 
 $app->post('/kpidept_upload', function(Request $request, Response $response) {
 	try{
+		$config = parse_ini_file("../src/config.ini");
+		$directory =  $config["upload_directory"];
 	  $uploadedFiles = $request->getUploadedFiles();
-		var_dump($uploadedFiles);
-	  // handle multiple inputs with the same key
-	  foreach ($uploadedFiles['file'] as $uploadedFile) {
+
+		foreach ($uploadedFiles as $uploadedFile) {
       if ($uploadedFile->getError() === UPLOAD_ERR_OK) {
-          $filename = moveUploadedFile($directory, $uploadedFile);
-          $response->getBody()->write('uploaded ' . $filename . '<br/>');
+        $filename = moveUploadedFile($directory, $uploadedFile);
+        $response->getBody()->write('uploaded ' . $directory . DIRECTORY_SEPARATOR . $filename . '<br/>');
       }
-	  }
+    }
+
 		return $response;
 	}catch(Exception $e){
 		$msg = $e->getMessage();
-		$response->getBody()->write($msg);
+		$response->getBody()->write($msg . ' directory ' . $directory);
 		return $response->withStatus(500)
 			->withHeader('Content-Type', 'text/html');
 	}
 });
+
+
+function moveUploadedFile($directory, UploadedFileInterface $uploadedFile)
+{
+	$filename = $uploadedFile->getClientFilename();
+	$uploadedFile->moveTo($directory . DIRECTORY_SEPARATOR . $filename);
+	return $filename;
+}
