86e27e1340056b047eec3cd9c5d42b70530833d9
fmanda <febrian.manda@gmail.com>
2020-08-19 19:32:20 +0700

    -

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -177,7 +177,7 @@ const scrollBehavior = (to, from, savedPosition) => {
 };
 
 const createRouter = () => new Router({
-  mode: 'history', // require service support
+  // mode: 'history', // require service support
   // scrollBehavior: () => ({ y: 0 }),
   scrollBehavior,
   routes: constantRoutes
